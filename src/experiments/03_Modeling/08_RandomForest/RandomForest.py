f4b055c05afd8c79f417a052edc768952513ef72
srechberger <stefan_rechberger@gmx.at>
2021-04-30 17:43:13 +0200

    Modeling (RandomForestRegressor) - single store model (708)

--- src/experiments/03_Modeling/08_RandomForest/RandomForest.py
+++ src/experiments/03_Modeling/08_RandomForest/RandomForest.py
@@ -13,6 +13,10 @@ def rmse(x, y):
 # All Stores
 X_train = pd.read_pickle('../../../../data/rossmann/intermediate/03_SalesModelingBase/01_AllStores/train_X.pkl')
 y_train = pd.read_pickle('../../../../data/rossmann/intermediate/03_SalesModelingBase/01_AllStores/train_y.pkl')
+# Store 708
+X_train_708 = pd.read_pickle('../../../../data/rossmann/intermediate/03_SalesModelingBase/02_Store708/train_store708_X.pkl')
+y_train_708 = pd.read_pickle('../../../../data/rossmann/intermediate/03_SalesModelingBase/02_Store708/train_store708_y.pkl')
+
 
 # Get Test Data
 # All Stores
@@ -58,6 +62,27 @@ print("All Stores")
 print("Training RMSE", ":", rmse(y_train, y_train_predicted),
       "Testing RMSE", ":", rmse(y_test, y_test_predicted))
 
+# ----- ++++ separate training attempt ++++ -----
+
+# Fit Model
+rf_model_708 = RandomForestRegressor(n_estimators=30)
+rf_model_708.fit(X_train, y_train)
+
+print("Regresion Model Score", ":", rf_model_708.score(X_train_708, y_train_708), ",",
+      "Out of Sample Test Score", ":", rf_model_708.score(X_test_3M_708, y_test_3M_708))
+
+# Prediction
+y_train_pre_708 = rf_model_708.predict(X_train_708)
+y_test_pre_2W_708 = rf_model_708.predict(X_test_2W_708)
+y_test_pre_1M_708 = rf_model_708.predict(X_test_1M_708)
+y_test_pre_3M_708 = rf_model_708.predict(X_test_3M_708)
+
+# Evaluation
+print("Predictions with Single Store Information")
+print("Training RMSE", ":", rmse(y_train_708, y_train_pre_708))
+print("Testing 2W RMSE", ":", rmse(y_test_2W_708, y_test_pre_2W_708))
+print("Testing 1M RMSE", ":", rmse(y_test_1M_708, y_test_pre_1M_708))
+print("Testing 3M RMSE", ":", rmse(y_test_3M_708, y_test_pre_3M_708))
 
 # -------------------------- Store 708 ------------------------------------------
 
